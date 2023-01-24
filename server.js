ec71264973dbaeafe2115a801d723b399b007592
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-01-24 13:01:33 +0530

    deleting patient

--- server.js
+++ server.js
@@ -6,98 +6,112 @@ import doctors from './dbDoctors.js'
 import users from './dbUsers.js'
 import Cors from "cors"
 
-const app=express()
-const port= process.env.port || 8001
+const app = express()
+const port = process.env.port || 8001
 const connectionUrl = 'mongodb+srv://admin:5OqQw0B1zLNvDhYt@cluster0.npypzlq.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(express.json())
 app.use(Cors())
 
-mongoose.connect(connectionUrl,{
-    useNewUrlParser:true,
+mongoose.connect(connectionUrl, {
+    useNewUrlParser: true,
     // useCreateIndex:true,
-    useUnifiedTopology:true
+    useUnifiedTopology: true
 })
 
-app.get("/",(req,res)=>res.status(200).send("wassup doc"))
+app.get("/", (req, res) => res.status(200).send("wassup doc"))
 
-app.get('/rmp',(req,res)=>{
-    freeDocNotifications.find((err,data)=>{
-        if(err){
+app.get('/rmp', (req, res) => {
+    freeDocNotifications.find((err, data) => {
+        if (err) {
             res.status(500).send(err)
         }
-        else{
+        else {
             res.status(200).send(data)
         }
     })
 })
 
-app.post('/rmp',(req,res)=>{
-    const freeDocNotification=req.body
-    freeDocNotifications.create(freeDocNotification,(err,data)=>{
-        if (err){
+app.post('/rmp', (req, res) => {
+    const freeDocNotification = req.body
+    freeDocNotifications.create(freeDocNotification, (err, data) => {
+        if (err) {
             res.status(500).send(err)
         }
-        else{
+        else {
             res.status(201).send(data)
         }
     })
 })
 
-app.post('/doc',(req,res)=>{
-    const patientToday=req.body
-    patientsToday.create(patientToday,(err,data)=>{
-        if (err){
+// app.delete('/doc',(req,res)=>{
+//     try{
+//         patientsToday.remove({_id: mongodb.ObjectID( req.params.id)});
+//         return res.status(200).json({ success: true, msg: `Product Deleted ${req.params.id}` });
+//     }
+//     catch(err){
+//         console.error(err);
+//     }
+// })
+
+app.delete('/doc/:id', function (req, res) {
+    let deleteID = req.params.id
+    patientsToday.findOneAndDelete({ _id: deleteID }, function (err, doc) {
+        if (err) {
+            res.send(err)
+        } else {
+            if (doc == null) {
+                res.send("Wrong id")
+            } else {
+                res.send(doc)
+            }
+        }
+
+    });
+})
+
+app.get('/doc', (req, res) => {
+    patientsToday.find((err, data) => {
+        if (err) {
             res.status(500).send(err)
         }
-        else{
+        else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/login', (req, res) => {
+    users.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.get('/booking', (req, res) => {
+    doctors.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/booking', (req, res) => {
+    const doctorsList = req.body
+    patientsToday.create(doctorsList, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        }
+        else {
             res.status(201).send(data)
         }
     })
 })
-
-app.get('/doc',(req,res)=>{
-    patientsToday.find((err,data)=>{
-        if(err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(200).send(data)
-        }
-    })
-})
-
-app.get('/login',(req,res)=>{
-    users.find((err,data)=>{
-        if(err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(200).send(data)
-        }
-    })
-})
-
-app.get('/booking',(req,res)=>{
-    doctors.find((err,data)=>{
-        if(err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(200).send(data)
-        }
-    })
-})
-
-app.post('/booking',(req,res)=>{
-    const doctorsList=req.body
-    doctors.create(doctorsList,(err,data)=>{
-        if (err){
-            res.status(500).send(err)
-        }
-        else{
-            res.status(201).send(data)
-        }
-    })
-})
-app.listen(port , ()=> console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`))
